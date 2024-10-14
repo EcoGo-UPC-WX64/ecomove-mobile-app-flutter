8edb06ed5e6ebfa545ac152f8e4b31872072c6a6
Augusto Pin <augustopm607@gmail.com>
2024-10-14 18:15:30 -0500

    feat: add security alert and support view

---
+++ lib/customer_support/alerta_seguridad.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class AlertaSeguridadPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  'ALERTAS',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Avisos de seguridad',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  bulletPoint(
+                      'Siempre use casco y equipo de protección al andar en bicicletas o scooters eléctricos.'),
+                  bulletPoint(
+                      'Respete las normas de tráfico y circule por carriles designados cuando esté en la vía pública.'),
+                  bulletPoint(
+                      'Antes de comenzar su viaje, asegúrese de que la bicicleta o el scooter estén en condiciones seguras de funcionamiento.'),
+                ],
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Consejos de uso de vehículos',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  bulletPoint(
+                      'Antes de comenzar su viaje, verifique la carga de la batería para evitar quedarse sin energía en el camino.'),
+                  bulletPoint(
+                      'Mantenga una velocidad segura y adecuada para las condiciones del entorno en el que se encuentre.'),
+                  bulletPoint(
+                      'Estacione los vehículos en áreas designadas y seguras para evitar obstrucciones y garantizar la accesibilidad para otros usuarios.'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.lightBlue[50],
+    );
+  }
+
+  Widget bulletPoint(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("• ", style: TextStyle(fontSize: 16)),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 16.0),
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:ecomove_flutter_mobile/customer_support/alerta_seguridad.dart';
+import 'package:ecomove_flutter_mobile/customer_support/soporte.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      //home: SoportePage(),
+      home: AlertaSeguridadPage(),
     );
   }
 }
