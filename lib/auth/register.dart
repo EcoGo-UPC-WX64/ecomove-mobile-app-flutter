76e14d93fe52269515f62a48e7add6b61b43a0c5
Angello Sosa <149955558+AngelloSosa@users.noreply.github.com>
2024-10-21 13:19:37 -0500

    feat: Add api and funcionalities in register

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -1,17 +1,69 @@
-import 'package:ecomove_flutter_mobile/auth/register_confirmation.dart';
 import 'package:flutter/material.dart';
+import 'login.dart';
+import '/services/api_service.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+  ApiService apiService = ApiService();
+
+  bool isLoading = false;
+
+  Future<void> _register() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    if (passwordController.text != confirmPasswordController.text) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Las contraseñas no coinciden")),
+      );
+      setState(() {
+        isLoading = false; // Ocultar indicador de carga
+      });
+      return;
+    }
+
+    final userData = {
+      'firstName': firstNameController.text,
+      'lastName': lastNameController.text,
+      'username': usernameController.text,
+      'email': emailController.text,
+      'password': passwordController.text,
+    };
+
+    try {
+      // Llama al servicio API para registrar al usuario
+      final response = await apiService.registerUser(userData);
+      print('Registro exitoso: $response');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Registro exitoso")),
+      );
+    } catch (e) {
+      print('Error en el registro: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error al registrarse")),
+      );
+    } finally {
+      setState(() {
+        isLoading = false; // Ocultar indicador de carga
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFE6F4FB), // Color de fondo del cuerpo
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFE6F4FB), // Color de fondo de la AppBar
-        elevation: 0, // Sin sombra bajo la AppBar
-        automaticallyImplyLeading: true,
-      ),
-
+      backgroundColor: Colors.lightBlue[50],
       body: Center(
         child: Card(
           elevation: 4,
@@ -20,13 +72,13 @@ class Register extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(16.0), // Padding para la Card
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 ListTile(
                   title: Padding(
-                    padding: const EdgeInsets.all(10), // Menos padding aquí
+                    padding: const EdgeInsets.all(10),
                     child: Text(
                       'Regístrate',
                       textAlign: TextAlign.center,
@@ -37,70 +89,105 @@ class Register extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: 20), // Espacio entre el título y el campo de texto
+                SizedBox(height: 20),
                 TextField(
+                  controller: firstNameController,
                   decoration: InputDecoration(
                     hintText: 'Nombres',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
-                    ), // Borde alrededor del campo
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10), // Padding interno
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
                 ),
-                SizedBox(height: 20), // Espacio entre el campo de texto y el botón
+                SizedBox(height: 20),
                 TextField(
+                  controller: lastNameController,
                   decoration: InputDecoration(
                     hintText: 'Apellidos',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
-                    ), // Borde alrededor del campo
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10), // Padding interno
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
                 ),
-                SizedBox(height: 20), // Espacio entre el campo de texto y el botón
+                SizedBox(height: 20),
                 TextField(
+                  controller: usernameController,
                   decoration: InputDecoration(
                     hintText: 'Nombre de Usuario',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
-                    ), // Borde alrededor del campo
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10), // Padding interno
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
                 ),
-                SizedBox(height: 20), // Espacio entre el campo de texto y el botón
+                SizedBox(height: 20),
                 TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Correo Electrónico',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Contraseña',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
-                    ), // Borde alrededor del campo
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10), // Padding interno
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
                 ),
-                SizedBox(height: 20), // Espacio entre el campo de texto y el botón
+                SizedBox(height: 20),
                 TextField(
+                  controller: confirmPasswordController,
+                  obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Repetir Contraseña',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
-                    ), // Borde alrededor del campo
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10), // Padding interno
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                   ),
                 ),
-                SizedBox(height: 20), // Espacio adicional al final si deseas agregar más elementos
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterConfirmation()) ,
-                    );
-                  },
-                  child: Text('Ingresar'),
+                SizedBox(height: 20),
+                isLoading
+                    ? CircularProgressIndicator() // Mostrar indicador de carga
+                    : ElevatedButton(
+                  onPressed: _register,
+                  child: Text('Registrarse'),
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF4F889E),
+                    backgroundColor: Colors.cyan[800],
                     foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25), // Padding del botón
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(50),
-                    ), // Bordes del botón
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Login(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Ya tengo una cuenta',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.cyan[800],
+                    ),
                   ),
                 ),
                 SizedBox(height: 20),
@@ -110,5 +197,17 @@ class Register extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // Libera los controladores cuando ya no son necesarios
+    firstNameController.dispose();
+    lastNameController.dispose();
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
   }
 }
