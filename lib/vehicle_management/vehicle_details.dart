0ded0f54ac95a38ee62a3cc4a252f24859914a51
Marcelo rentería <u202216558@upc.edu.pe>
2024-11-07 12:21:51 -0500

    feat: Created the Vehicle Details file, with a placeholder.

---
+++ lib/vehicle_management/vehicle_details.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VehicleDetails extends StatefulWidget {
+  @override
+  State<VehicleDetails> createState() => _VehicleDetailsState();
+}
+
+class _VehicleDetailsState extends State<VehicleDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
