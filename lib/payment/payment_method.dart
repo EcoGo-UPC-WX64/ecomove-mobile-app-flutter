05e12712beb2d569ddf9905b81bcc6a49b20cd02
Angello Sosa <149955558+AngelloSosa@users.noreply.github.com>
2024-11-07 00:37:45 -0500

    feat: fix name conventions

--- lib/payment/payment_method.dart
+++ lib/payment/payment_method.dart
@@ -1,7 +1,7 @@
 import 'package:ecomove_flutter_mobile/shared/custom_returnAppBar.dart';
 import 'package:ecomove_flutter_mobile/user_management/profile.dart';
 import 'package:flutter/material.dart';
-import 'registro_tarjeta.dart';
+import 'car_register.dart';
 
 class MetodoPagoPage extends StatelessWidget {
   @override
