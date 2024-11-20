33ddcc6a3e72519b5c18227027af019c21687cb4
Kuni_2005 <math.kuni123@gmail.com>
2024-11-20 15:11:23 -0500

    feat: Added T&C

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -17,18 +17,28 @@ class _RegisterState extends State<Register> {
   ApiService apiService = ApiService();
 
   bool isLoading = false;
+  bool acceptTerms = false; // Estado del checkbox
 
   Future<void> _register() async {
+    // Validar si el usuario aceptó los términos
+    if (!acceptTerms) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Debes aceptar los términos y condiciones para registrarte")),
+      );
+      return;
+    }
+
     setState(() {
       isLoading = true;
     });
 
+    // Validar si las contraseñas coinciden
     if (passwordController.text != confirmPasswordController.text) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Las contraseñas no coinciden")),
       );
       setState(() {
-        isLoading = false; // Ocultar indicador de carga
+        isLoading = false;
       });
       return;
     }
@@ -42,7 +52,6 @@ class _RegisterState extends State<Register> {
     };
 
     try {
-      // Llama al servicio API para registrar al usuario
       final response = await apiService.registerUser(userData);
       print('Registro exitoso: $response');
       ScaffoldMessenger.of(context).showSnackBar(
@@ -55,7 +64,7 @@ class _RegisterState extends State<Register> {
       );
     } finally {
       setState(() {
-        isLoading = false; // Ocultar indicador de carga
+        isLoading = false;
       });
     }
   }
@@ -65,19 +74,19 @@ class _RegisterState extends State<Register> {
     return Scaffold(
       backgroundColor: Colors.lightBlue[50],
       body: Center(
-        child: Card(
-          elevation: 4,
-          margin: EdgeInsets.all(40),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  title: Padding(
+        child: SingleChildScrollView(
+          child: Card(
+            elevation: 4,
+            margin: EdgeInsets.all(40),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
                     padding: const EdgeInsets.all(10),
                     child: Text(
                       'Regístrate',
@@ -88,110 +97,140 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: firstNameController,
-                  decoration: InputDecoration(
-                    hintText: 'Nombres',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: lastNameController,
-                  decoration: InputDecoration(
-                    hintText: 'Apellidos',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                    hintText: 'Nombre de Usuario',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: 'Correo Electrónico',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Contraseña',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: confirmPasswordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Repetir Contraseña',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                  ),
-                ),
-                SizedBox(height: 20),
-                isLoading
-                    ? CircularProgressIndicator() // Mostrar indicador de carga
-                    : ElevatedButton(
-                  onPressed: _register,
-                  child: Text('Registrarse'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.cyan[800],
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Login(),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: firstNameController,
+                    decoration: InputDecoration(
+                      hintText: 'Nombres',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
                       ),
-                    );
-                  },
-                  child: Text(
-                    'Ya tengo una cuenta',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.cyan[800],
+                      contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-              ],
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: lastNameController,
+                    decoration: InputDecoration(
+                      hintText: 'Apellidos',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      hintText: 'Nombre de Usuario',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: 'Correo Electrónico',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Contraseña',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: confirmPasswordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Repetir Contraseña',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: acceptTerms,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            acceptTerms = value ?? false;
+                          });
+                        },
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("Lee nuestros términos y condiciones")),
+                            );
+                          },
+                          child: Text(
+                            'Acepto los términos y condiciones',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  isLoading
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: _register,
+                          child: Text('Registrarse'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.cyan[800],
+                            foregroundColor: Colors.white,
+                            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                        ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Login(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Ya tengo una cuenta',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.cyan[800],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
           ),
         ),
@@ -201,7 +240,6 @@ class _RegisterState extends State<Register> {
 
   @override
   void dispose() {
-    // Libera los controladores cuando ya no son necesarios
     firstNameController.dispose();
     lastNameController.dispose();
     usernameController.dispose();
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/user_provider.dart';
 
-
 void main() {
   runApp(
     MultiProvider(
